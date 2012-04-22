4b7c3f41345ab0a07c5e5bcdf82d3650fe06ed96
Richard Futrell <canjobear@gmail.com>
2012-04-22 15:04:53 -0700

    tweaked prepareSVMFeatures

--- prepareSVMFeatures.py
+++ prepareSVMFeatures.py
@@ -5,7 +5,7 @@ userInput = sys.argv[1:]
 v = Vectors()
 for docName in userInput:
     docName = "op_spam_v1.3/all/" + docName
-    print "Adding document " + docName
+    #print "Adding document " + docName
     docText = [s for s in open(docName)]
     if len(docText) > 0:
         docText = docText[0]
