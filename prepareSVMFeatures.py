0c49f71796dbeb1ec7a2215d6d8eee39683b32b7
Richard Futrell <canjobear@gmail.com>
2012-04-22 15:14:31 -0700

    now properly appends class +1 or -1 automatically to the vector representation of a doc

--- prepareSVMFeatures.py
+++ prepareSVMFeatures.py
@@ -4,11 +4,18 @@ from text2vec import Vectors
 userInput = sys.argv[1:]
 v = Vectors()
 for docName in userInput:
-    docName = "op_spam_v1.3/all/" + docName
+    #docName = "op_spam_v1.3/all/" + docName
     #print "Adding document " + docName
     docText = [s for s in open(docName)]
     if len(docText) > 0:
         docText = docText[0]
         v.addDoc(docName,docText)
 v.saveFeatures()
-v.printAllVectors()
+
+for docName in v.docs.keys():
+    s = v.vectorString(v.docs[docName])
+    if "d_" in s:
+        print "+1 " + s
+    if "t_" in s:
+        print "-1 " + s
+
