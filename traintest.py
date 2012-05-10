d4ff1f11c6c73ea7e857c328d60033bd134f71fb
Richard Futrell <canjobear@gmail.com>
2012-05-10 00:53:46 -0700

    Output/readss intermediate vectors to a file now, not a directory

--- traintest.py
+++ traintest.py
@@ -3,36 +3,10 @@
 import sys
 import os
 import ClassifierTrainTest
-import produceVectors
-from text2vec import Vectors
+import cPickle as pickle
+import text2vec
 import argparse
-
-def readVectorFile(filename, v):
-    infile = open(filename)
-    if ClassifierTrainTest.classifier.__name__ == 'svmlight':
-        toReturn = v.string2pysvmVector(infile.read())
-    else:
-        toReturn = v.string2nltkVector(infile.read())
-    infile.close()
-    return toReturn
-
-def readVectorDir(pathname):
-    if pathname[-1] is not '/':
-        pathname += '/'
-
-    v = Vectors()
-    v.loadFeatures('dat.features')
-
-    files = os.listdir(pathname)
-    vectors = [readVectorFile(pathname+f, v) for f in files]
-
-def getVectors(v):
-    if ClassifierTrainTest.classifier.__name__ == 'svmlight':
-        return v.allPysvmVectors()
-    else: 
-        return v.allDocs()
-
-
+import nltk
 
 
 parser = argparse.ArgumentParser(description='k-fold cross validation of SVM classification of text files')
@@ -40,8 +14,8 @@ parser.add_argument('dir',type=str,default='dbyank/',help='directory with traini
 parser.add_argument('-k','--folds',type=int,default=5,help='number of folds for number of folds for cross-validation, default 5',action='store')
 parser.add_argument('-n','--ngramOrder',type=int,default=3,help='ngram order, default 3',action='store')
 parser.add_argument('-p','--posgramOrder',type=int,default=0,help='ngram order, default 0',action='store')
-parser.add_argument('-o','--outputVectors',type=str,default='',help='directory to Output vector representations of documents into. By default, vectors are not printed.',action='store')
-parser.add_argument('-v','--vectors',type=str,default='',help='directory of vectors to read. If no directory is selected, vectors will be generated from text files in [DIR].',action='store')
+parser.add_argument('-o','--outputVectors',type=str,default='',help='file to Output vector representations of documents into. By default, vectors are not printed.',action='store')
+parser.add_argument('-v','--inputVectors',type=str,default='',help='file of Vectors to read. If no file is selected, vectors will be generated from text files in [DIR].',action='store')
 parser.add_argument('-t','--test',type=str,default='',help='directory of documents for testing.',action='store')
 parser.add_argument('-c','--cutoff',type=float,default=0,help='cutoff value for classification as fake',action='store')
 parser.add_argument('-b','--balanced',type=bool,default=True,help='whether categories in folds should be balanced',action='store')
@@ -54,16 +28,20 @@ pathname = vars(args)['dir']
 howManyFolds = vars(args)['folds']
 ngramOrder = vars(args)['ngramOrder']
 posgramOrder = vars(args)['posgramOrder']
-outdir = vars(args)['outputVectors']
-indir = vars(args)['vectors']
+outfilename = vars(args)['outputVectors']
+infilename = vars(args)['inputVectors']
 testdir = vars(args)['test']
 balanced = vars(args)['balanced']
 addK = vars(args)['addK']
 ClassifierTrainTest.pivot = vars(args)['cutoff']
 if vars(args)['classifier'] == 'nb':
-    import NaiveBayes as nb
-    nb.addK = addK
-    ClassifierTrainTest.setClassifier(nb)
+    ClassifierTrainTest.setClassifier(nltk.NaiveBayesClassifier)
+elif vars(args)['classifier'] == 'maxent':
+    ClassifierTrainTest.setClassifier(nltk.classify.maxent.MaxentClassifier)
+else:
+    import svmlight
+    ClassifierTrainTest.setClassifier(svmlight)
+    
 
 
 if not pathname[-1] == '/':
@@ -75,27 +53,21 @@ params = dict()
 params['ngramOrder'] = ngramOrder
 params['posgramOrder'] = posgramOrder
 vectors = []
-if not outdir == '': # If a vector-output directory was specified
-    if not outdir[-1] == '/':
-        outdir += '/'
 
-    produceVectors.makeVectorsFromFiles(pathname, outdir, **params)
-    vectors = readVectorDir(pathname)
+if not infilename == '': # If already-produced Vectors file was specified
+    infile = open(infilename)
+    docs = pickle.load(infile)
+    infile.close()
 
-elif not indir == '': # If already-produced vector files were specified
-    vectors = readVectorDir(pathname)
+else: # If no vectors file has been specified, generate from text files
 
-else:
-    v = Vectors(**params)
+    docs = []
     for docName in os.listdir(pathname):
-        docFile = open(pathname+docName,'r')
-        docText = docFile.read()
-        if len(docText) > 0:
-            v.addText(docText,docName)
-        docFile.close()
-    v.saveFeatures()
-
-    vectors = getVectors(v)
+        docs.append(text2vec.file2doc(docName,pathname))
+    if not outfilename == '': # if output file specified
+        outfile = open(outfilename,'w')
+        pickle.dump(docs,outfile)
+        outfile.close()
 
 
 ###### TRAIN & TEST AN SVM #####
@@ -104,18 +76,12 @@ if not testdir == '':   # If test documents specified
     if testdir[-1] is not '/':
         testdir += '/'
 
-    v = Vectors(**params)
-    v.loadFeatures('dat.features')
     testdocs = []
-    if ClassifierTrainTest.classifier.__name__ == 'svmlight':
-        for docName in os.listdir(pathname):
-            testdocs.append(v.file2pysvm(pathname+docName))
-    else:
-        for docName in os.listdir(pathname):
-            testdocs.append(v.file2doc(pathname+docName))
+    for docName in os.listdir(testdir):
+        testdocs.append(text2vec.file2doc(docName, testdir))
         
 
-    result = ClassifierTrainTest.trainAndTest(vectors, testdocs)
+    result = ClassifierTrainTest.trainAndTest(docs, testdocs)
     ClassifierTrainTest.printSummary(result)
 
     outfile = open('output-details','w')
@@ -124,4 +90,4 @@ if not testdir == '':   # If test documents specified
     
     
 else:
-    results = ClassifierTrainTest.kFoldTrainAndTest(vectors, howManyFolds, balanced)
+    results = ClassifierTrainTest.kFoldTrainAndTest(docs, howManyFolds, balanced)
